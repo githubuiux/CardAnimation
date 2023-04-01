7e73d31539b135e7bc7f663438564d7499b6d7aa
githubuiux <uiuxista@gmail.com>
2023-04-01 12:23:14 +0300

    Initial Commit

---
+++ CardAnimation/CardAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAnimationApp.swift
+//  CardAnimation
+//
+//  Created by Levent on 1.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CardAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
